324967897313adb4210f5537b913df6b9efd6d91
John Sullivan <johnsully83@gmail.com>
2021-01-06 13:33:13 -0500

    Update validate-number-of-submissions.ts
    Adding comments for code

--- src/page-interactions/record/add-edit-presave/validate-number-of-submissions.ts
+++ src/page-interactions/record/add-edit-presave/validate-number-of-submissions.ts
@@ -13,8 +13,20 @@ const interaction: PageInteraction = {
                 const jobOrderId = form.jobOrder.id;
 
                 var max = myClient.getMaxSubmissions(jobOrderId);
-                var num = myClient.queryNumberOfSubmissions(jobOrderId);
-
+                var num = myClient.queryNumberOfSubmissions(jobOrderId);//need to be handling the return Promise from both of these function calls here.  should look something like the below
+                /*
+                return myClient.getMaxSubmissions(jobOrderId).then(response => {
+                    const max = response.data.data.customText9;
+                    
+                    return myClient.queryNumberOfSubmissions(jobOrderId).then(response => {
+                      const num = response.data.total; // here we use that total property referenced below
+                      
+                        ...the rest of your code would go in here and should behave fine
+                        
+                        return form;// finally return the form in the promise to resolve it
+                    });
+                })
+                */
                 if(num < max)
                 {
                     form.valid =  true;
@@ -29,15 +41,16 @@ const interaction: PageInteraction = {
 
             
             getMaxSubmissions: (jobOrderId) => {
-                var maxSub = API.appBridge.httpGET('/entity/JobOrder/?clientCorporation(customText9)')
+                var maxSub = API.appBridge.httpGET('/entity/JobOrder/?clientCorporation(customText9)');
+                //  URL should be `/entity/JobOrder/${jobOrderId}?fields=clientCorporation(customText9)`
                 return maxSub;
             },
 
             queryNumberOfSubmissions: (jobOrderId) => {
                 const where = 'jobOrder.id = ${jobOrderId}';
 
-                var numSub = API.appBridge.httpGET('/query/JobSubmissions?where=${where}&fields=id')
-
+                var numSub = API.appBridge.httpGET('/query/JobSubmissions?where=${where}&fields=id')// JobSubmission should be singular in the URL, `/query/JobSubmission...`
+                // also you want to pass &showTotalMatched=true so the /query operation returns a 'total' property representing the total number of records
                 return numSub;
             }
         };
