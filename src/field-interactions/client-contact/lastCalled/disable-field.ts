10a7435c899a8cbb0542abc2d08a51ea87238f36
John Sullivan <johnsully83@gmail.com>
2021-01-08 09:59:11 -0500

    Update disable-field.ts

--- src/field-interactions/client-contact/lastCalled/disable-field.ts
+++ src/field-interactions/client-contact/lastCalled/disable-field.ts
@@ -14,7 +14,7 @@ const interaction: FieldInteraction = {
         init: () => {
             const userTypeId = API.globals.user.userTypeId;
 
-            if(userTypeId!==myClient.adminUserTypeId)
+            if(userTypeId!==myClient.adminUserTypeId)// only this to watch out for here is the !== as opposed to !=...I believe API.globals.user.userTypeId is a number (the comment you made above would solve this)
             {
                 API.disable('customDate1');
             }
