8d0f01b3ae8b194ca010d768bf7552de0a1be79b
John Sullivan <johnsully83@gmail.com>
2021-01-08 09:56:11 -0500

    Update copy-details-customText19.ts

--- src/field-interactions/client-contact/CompanyRecords/copy-details-customText19.ts
+++ src/field-interactions/client-contact/CompanyRecords/copy-details-customText19.ts
@@ -11,8 +11,10 @@ const interaction: FieldInteraction = {
         change: () => {
           const customText19 = API.getValue('customText19');
 
-          if(customText19!==undefined) {
+          if(customText19!==undefined) {// capital httpGET
               API.appBridge.httpGet('/entity/ClientContact/${customText19.id}?fields=email,phone').then( response => {
+                // in this case the value of customText19 is likely to just be the ID itself...it's the kind of thing you'd want to test out
+                // so instead of ${customText19.id} i believe it would be just ${customText19}
                 API.setValue('customText18 ',response.data.data.email);
                 API.setValue('customText20 ',response.data.data.phone);
                 
